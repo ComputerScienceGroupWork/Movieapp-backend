936eaa7005512afdd79a8e6fef512bd5d9264128
Papatee04 <128583063+Papatee04@users.noreply.github.com>
2023-05-05 15:46:54 +0200

    Update movies.js
    optimized the code using the promote all function to execute the code simultaneously to reduce response time and memory use.  from line 21 to line 60

--- api/movies.js
+++ api/movies.js
@@ -18,31 +18,47 @@ router.post('/', async(req,res)=>
     }
 })
 //gets a movie by id
-router.get('/:id',async({params:{id}},res)=>
-{
-    try
-    {
-        console.log(id)
-        let movie = await RatedMovie.findById(id).exec()
-        let reviews = await Review.find({movieId:id}).exec()
-        let ratings = await Rating.find({movieId:id}).exec()
-        let avgScore = 0;
-        let sum = 0;
-        ratings.forEach(element => {
-            sum += element.rating;
-        });
-        avgScore = sum/ratings.length
-        let res_data = {
-            movie: movie,
-            rating: avgScore,
-            reviews: reviews,
+router.get('/:id', async ({params: {id}}, res) => {
+    try {
+        console.log(id);
+
+        // Execute all queries in parallel
+        const [movie, reviews, ratings] = await Promise.all([
+            RatedMovie.findById(id).exec(),
+            Review.find({movieId: id}).exec(),
+            Rating.aggregate([
+                {$match: {movieId: id}},
+                {$group: {_id: null, avgRating: {$avg: "$rating"}}},
+            ]).exec(),
+        ]);
+
+        // Extract the average rating from the aggregation result
+        const avgScore = ratings.length > 0 ? ratings[0].avgRating : 0;
+
+        // Send the response as a streaming response
+        res.setHeader('Content-Type', 'application/json');
+        res.write('{"movie":');
+        res.write(JSON.stringify(movie));
+        res.write(',"rating":');
+        res.write(JSON.stringify(avgScore));
+        res.write(',"reviews":[');
+        let isFirstReview = true;
+        for await (const review of reviews) {
+            if (!isFirstReview) {
+                res.write(',');
+            }
+            res.write(JSON.stringify(review));
+            isFirstReview = false;
         }
-        res.json(res_data);
+        res.write(']}');
+        res.end();
+        
         console.log("Done");
-    }catch{
+    } catch {
         res.status(400).json("oops something went wrong");
     }
-})
+});
+
 
 
 //Add a rating to the movie
