7419523860134335477a29d7f2eac11ad280e424
wazabanda <1wazabanda@gmail.com>
2023-05-29 13:49:22 +0200

    Update movies.js

--- api/movies.js
+++ api/movies.js
@@ -12,7 +12,7 @@ router.post('/', async (req, res) => {
         return res.status(400).json({ "message": "user not authenticated" })
         //res.end()
     }
-	if (!RatedMovie.exists({ movie: req.body})) {
+	if (!RatedMovie.exists(req.body)) {
 
 		console.log(req.body)
 		let movie = new RatedMovie(req.body)
